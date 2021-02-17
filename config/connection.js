0499c210982942130c33f42639399259d6121ca6
Sam Rogers <samrogers15@gmail.com>
2021-02-17 14:46:31 -0800

    updating connection with Heroku info

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,24 @@ const util = require('util');
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.password,
-    database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: process.env.password,
+        database: 'burgers_db',
+    })
+};
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: process.env.password,
+//     database: 'burgers_db',
+// });
 
 connection.connect((err) => {
     if (err) throw err;
